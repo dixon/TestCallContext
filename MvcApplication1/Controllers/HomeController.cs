b451f467a6cabe2e1714925b53880f5c92284880
Jarrod Dixon <jarrod.m.dixon@gmail.com>
2013-10-31 00:25:53 -0400

    initial commit

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : AsyncController
+    {
+        public ActionResult Index()
+        {
+            var html = new StringBuilder()
+                .Append("<html><head><title>TestCallContext</title></head><body>")
+                .AppendFormat("<h2>Home/Index on {0}</h2>", HostAndPort)
+                .Append("<div>Make sure the debugger is attached and watch the Output -> Debug window for log messages</div>");
+
+            new List<string> { "/Home/Long", "/Home/Long2" }.ForEach(link => html.AppendFormat("<div><a href='{0}{1}'>{1}</a></div>", HostAndPort, link));
+            
+            html.Append("</body></html>");
+
+            return Content(html.ToString());
+        }
+
+        public ActionResult Long()
+        {
+            Thread.Sleep(2000);
+            return Content("Long");
+        }
+
+        public async Task<ActionResult> Long2Async()
+        {
+            var resp = await HitLong();
+            return Content("AsyncLong: " + resp);
+        }
+
+        public async Task<string> HitLong()
+        {
+            using (var http = new HttpClient())
+            {
+                var resp = await http.GetAsync(HostAndPort + "/Home/Long");
+                return resp.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        private string HostAndPort // e.g. http://localhost:56665
+        {
+            get { return Request.Url.GetLeftPart(UriPartial.Authority); }
+        }
+        
+    }
+}
---
+++ MvcApplication1/Global.asax.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)] // config is in root web.config
+
+namespace MvcApplication1
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        static log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            log.Info("begin");
+            CallContext.LogicalSetData("data", "what was the path? " + Request.Url.PathAndQuery);
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            log.Info("end");
+            log.Info(CallContext.LogicalGetData("data") ?? "EMPTY CALL CONTEXT");
+        }
+    }
+}
